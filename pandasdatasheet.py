e69959ef38ca68a9ff04d91b3bbc2217d92e68c3
ItzReality <49762304+ItzReality@users.noreply.github.com>
2025-02-26 20:21:53 -0800

    Update pandasdatasheet.py

--- pandasdatasheet.py
+++ pandasdatasheet.py
@@ -7,20 +7,20 @@ df = pd.read_csv('store_sales.csv')
 total_sales_by_store = df.groupby('Store_Location')['Total_Sales'].sum()
 print(total_sales_by_store)
 
-
+#Compute the mean of the Price column
 average_price = df['Price'].mean()
 print(f"Average Price: {average_price:.2f}")
 
-
+#Group by Payment_Method and calculate total sales for each method.
 payment_method_sales = df.groupby('Payment_Method')['Total_Sales'].sum()
 print(payment_method_sales)
 
-
+#Transaction Count by Payment Method
 transaction_count_by_payment = df.groupby(['Store_Location', 'Payment_Method'])[
     'Transaction_ID'].count()
 print(transaction_count_by_payment)
 
-
+#Quantity Sold by Payment Method
 quantity_by_payment = df.groupby(['Store_Location', 'Payment_Method'])[
     'Quantity'].sum()
 print(quantity_by_payment)
